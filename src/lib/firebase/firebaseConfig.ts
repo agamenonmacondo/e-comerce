d691c25c40b13c21f28dc79198bf96974000f4b3
Agamenon Macondo <agamenonmacondo@gmail.com>
2025-08-25 21:30:15 +0000

    File changes

--- src/lib/firebase/firebaseConfig.ts
+++ src/lib/firebase/firebaseConfig.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase/firebaseConfig.ts
 // IMPORTANT: Replace with your actual Firebase project configuration values.
 // You can find these in your Firebase project console:
@@ -9,13 +10,12 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE"
-  // measurementId: "YOUR_MEASUREMENT_ID_HERE" // Optional
+  "projectId": "icommerce-ya7tu",
+  "appId": "1:930956898744:web:2191c784ca4948bc09df4d",
+  "storageBucket": "icommerce-ya7tu.appspot.com",
+  "apiKey": "AIzaSyBow19I7eMfTCBqDdfWoOA975_OeyNdMLw",
+  "authDomain": "icommerce-ya7tu.firebaseapp.com",
+  "messagingSenderId": "930956898744"
 };
 
 let app: FirebaseApp;
